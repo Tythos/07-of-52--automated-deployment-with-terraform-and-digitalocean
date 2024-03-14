0f8241bd7bcdda3ec78bb671bec77c01ed2869cd
Brian Kirkpatrick <bekirkpatrick@proton.me>
2024-03-13 22:07:02 -0700

    updating with explicit dependencies to enable project destruction

--- infrastructure/doproject.tf
+++ infrastructure/doproject.tf
@@ -9,4 +9,10 @@ resource "digitalocean_project" "doproject" {
     digitalocean_domain.dodomain.urn,
     digitalocean_spaces_bucket.dobucket.urn
   ]
+
+  depends_on = [
+    digitalocean_droplet.dodroplet,
+    digitalocean_domain.dodomain,
+    digitalocean_spaces_bucket.dobucket
+  ]
 }
